b39cff07e36bf4e643fefc6dff856cfe20a17b1f
Isaac Batista <disa.indus@gmail.com>
2014-12-02 23:11:30 -0600

    terminamos el ejemplo de fucion de vectores

--- src/ejemplos/lwp/vectores/sumvector/SumaVectores.java
+++ src/ejemplos/lwp/vectores/sumvector/SumaVectores.java
@@ -18,90 +18,71 @@ public class SumaVectores {
 
     public static void main(String[]args){
 
-	int n, m, nec, i, k, dif;
-	
-	Scanner leer = new Scanner(System.in);
-	
-	System.out.print("Número de elementos del vector A: ");
-	n=leer.nextInt();
-	System.out.print("Número de elementos del vector B: ");
-	m=leer.nextInt();
-	
+	int n, m, i, k, dif;
+		Scanner leer = new Scanner(System.in);
+		int[] c;
+		
 
-	
-	int a[] = new int [n];
-	
-	for(i=0;i<n;i++){
-		System.out.print("Elemento del vector A: ["+i+"]");
-		a[i]=leer.nextInt();
+		System.out.print("Número de elementos del vector A: ");
+		n=leer.nextInt();
+		System.out.print("Número de elementos del vector B: ");
+		m=leer.nextInt();
+		
+		int a[] = new int [n];
+		for(i=0;i<n;i++){
+			System.out.print("Elemento del vector A: ["+i+"]");
+			a[i]=leer.nextInt();
+		}
+		
+
+		int b[] = new int [m];
+		for(k=0;k<m;k++){
+			System.out.print("Elemento del vector B: ["+k+"]");
+			b[k]=leer.nextInt();
+		}
+		
+		//Con esto siempre vamos a copiar en C el vector mas grande.
+		//if(n>m){
+			int aux = 0;
+			dif = 0;
+			for (int d=0; d<m; d++){
+				aux = b[d];
+				if(!duplicado(aux, a)){
+					dif++;
+				}
+	 		}
+			System.out.println("El tamaño del array C sera "+ (dif+n));
+			
+			 c = new int [dif + n];
+                         
+                         System.arraycopy(a, 0, c, 0, a.length);
+                         
+                         aux=0;
+                         int g = 0;
+                         for(i = 0; i < b.length; i++){
+                             aux = b[i];
+                             if(!duplicado(aux, a)){
+                                 c[a.length+g] = aux;
+                                 g++;
+                             }
+                         }
+                         
+                         for (int j=0; j < c.length; j++) {
+                             System.out.println("C["+j+"]: "+c[j]);
+                         }
+    }
+    public static boolean duplicado(int a, int array[]){
+	int index;
+
+	Arrays.sort(array);
+        index=Arrays.binarySearch(array, a);
+	if(index < 0){
+            //Si NO se encuentra
+            return false;
+	}else if (index >= 0){
+            return true;
 	}
-	
-	int b[] = new int [m];
-	
-	for(k=0;k<m;k++){
-		System.out.print("Elemento del vector B: ["+k+"]");
-		b[k]=leer.nextInt();
-	}
-	
-	
-	//Con esto siempre vamos a copiar en C el vector mas grande.
-        if(n>m){
-//            for(i=0;i<n;i++){
-//                    c[i]=a[i];
-//            }
-//            int aux = 0;
-//            dif = 0;
-//            for (int d=0; d<m; d++){
-//                aux = b[d];
-//                //for(int e=0; e<a.length; e++){
-//                    if(!a.equals(aux)){
-//                        dif++;
-//                //    }
-//                }                
-//            }
-//            int c[] = new int[n+nec]; 
-//            for (i=0; i<c.length; i++){
-//                c[i] = a[i];
-//            }
-//            for (i = n; i<c.length;i++){
-//                
-//            }
-//            
-            Arrays.sort(a);
-            int aux = 0;
-            for (k=0; k<m; k++){
-                aux = b[k];
-                if(Arrays.equals(a, b)){
-                    
-                } else {
-                }
-            }
-            
-        }else{
-            for(i=0;i<m;i++){
-                    c[i]=b[i];
-            }   
-        }
-	
-//	nec=n-1;
-//	
-//	for(k=0;k<m;k++){
-//		i=0;
-//		while((i<n) && (b[k]!=a[i])){
-//			i=i+1;
-//			
-//		}
-//		if(i>n){
-//			nec=nec+1;
-//			c[nec]=b[k];
-//		}
-//	}
-//	
-//	//for(i=0;i<nec;i++){
-//	for(i=0;i<c.length;i++){
-//		System.out.println("Valor del vector C["+i+"]: "+c[i]);
-//	}
-//	
-	}
+        return false;
+    }
 }
 
